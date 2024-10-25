2e91fc89aef8172704edc8feca69e7ecc410a02e
kevin.orathel <kevin.orathel@gmail.com>
2024-10-25 18:18:53 -0400

    Feat: New feature changes

--- Application/views.py
+++ Application/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
 from .forms import LoginForm
 from .models import User
 
@@ -31,3 +37,28 @@ def login(request):
     else:
         form = LoginForm()
         return render(request, 'Application/login.html', {'form': form})
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = User.objects.filter(email=email).first()
+        if user:
+            token = default_token_generator.make_token(user)
+            reset_link = request.build_absolute_uri(f"/reset_password/{user.pk}/{token}/")
+            send_mail(
+                'Password Reset Request',
+                f'Click the link to reset your password: {reset_link}',
+                'noreply@example.com',
+                [email]
+            )
+            return JsonResponse({'status': 'success', 'message': 'Reset link sent!'})
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Email not found'})
+
+
+
+@login_required
+def my_profile(request):
+    user = request.user
+    return render(request, 'my_profile.html', {'user': user})
+
